9777d3b6f8ed30c4d64823ffa537d18c3eaa0c53
Brian Anders <anders.brian@icloud.com>
2014-06-11 14:52:02 -0700

    adjusted deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@
 echo "Changing build permissions to 755"
 chmod -R 755 build/
 echo "Moving Files to EC2"
-rsync -avz build/ briananders.me:/home/ec2-user/build-temp
-echo "Swapping build folders"
-ssh -t briananders.me 'mv build/ build-delete/; mv build-temp/ build/;'
+rsync -avz build/ briananders.me:/home/ec2-user/build
 echo "moving nginx config"
 rsync -avz default.conf briananders.me:/home/ec2-user
 echo "restarting nginx"
