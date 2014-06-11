6aea1c3ddcc735e0e4f7418643a86f8d253f5ad7
Brian Anders <anders.brian@icloud.com>
2014-06-11 11:50:13 -0700

    added nginx config deployment to deploy script flow

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ rsync -avz build/ briananders.me:/home/ec2-user/build-temp
 echo "Swapping build folders"
 ssh -t briananders.me 'mv build/ build-delete/; mv build-temp/ build/;'
 echo "moving nginx config"
+rsync -avz default.conf briananders.me:/home/ec2-user
 echo "restarting nginx"
+ssh -t briananders.me 'sudo /etc/init.d/nginx restart'
