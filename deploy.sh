c71f15df22d255f4808b1a623672db9d6d3399d1
Brian Anders <anders.brian@icloud.com>
2015-02-16 22:05:48 -0800

    New deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/zsh
 echo "Changing build permissions to 755"
 chmod -R 755 build/
-echo "Moving Files to EC2"
-rsync -avz build/ briananders.me:/home/ec2-user/build
-echo "moving nginx config"
-rsync -avz default.conf briananders.me:/home/ec2-user
-echo "restarting nginx"
-ssh -t briananders.me 'sudo /etc/init.d/nginx restart'
+echo "Moving Files to S3"
+aws s3 sync build/ s3://www.briananders.me --recursive
+echo "Done"
 
 # /bin/zsh deploy.sh
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -2,10 +2,7 @@
 echo "Changing build permissions to 755"
 chmod -R 755 build/
 echo "Moving Files to EC2"
-rsync -avz build/ briananders.me:/home/ec2-user/staging
-echo "moving nginx config"
-rsync -avz default.conf briananders.me:/home/ec2-user
-echo "restarting nginx"
-ssh -t briananders.me 'sudo /etc/init.d/nginx restart'
+aws s3 sync build/ s3://staging.briananders.me --recursive
+echo "Done"
 
 # /bin/zsh deploy.sh
