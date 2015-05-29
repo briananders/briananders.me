9ea9d46e1cfaf5a99af2ea06b2b0579e51a018c6
Brian Anders <anders.brian@icloud.com>
2015-05-29 11:17:18 -0700

    better content and js.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,9 @@
-//= require spin
+'use strict';
+
+/* globals $ */
+
+
+
 
 //ie8 doesn't have a forEach method. This fixes that.
 if ( !Array.prototype.forEach ) {
@@ -6,7 +11,7 @@ if ( !Array.prototype.forEach ) {
     for(var i = 0, len = this.length; i < len; ++i) {
       fn.call(scope, this[i], i, this);
     }
-  }
+  };
 }
 
 
@@ -15,17 +20,25 @@ if ( !Array.prototype.forEach ) {
 
 
 
-/*
- * .addClassSVG(className)
- * Adds the specified class(es) to each of the set of matched SVG elements.
- */
-$.fn.addClassSVG = function(className){
-    $(this).attr('class', function(index, existingClassNames) {
-        return existingClassNames + ' ' + className;
-    });
-    return this;
+
+
+var ImageWatcher = function() {
+
+  var images = [];
+
+
+  this.watchImage = function(index, img) {
+
+    images.push(img);
+
+    img.onload = function() {
+      $(this).addClass('loaded');
+    };
+  };
+
 };
 
+var imageWatcher = new ImageWatcher();
 
 
 
@@ -33,168 +46,63 @@ $.fn.addClassSVG = function(className){
 
 
 
-/*
- * .removeClassSVG(className)
- * Removes the specified class to each of the set of matched SVG elements.
- */
-$.fn.removeClassSVG = function(className){
-    $(this).attr('class', function(index, existingClassNames) {
-        var re = new RegExp(className, 'g');
-        return existingClassNames.replace(re, '');
-    });
-    return this;
-};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-(function($){
-  /*
-    makes the image_tag helper retina images work.
-  */
-  window.isRetina = false;
-
-  if(window.devicePixelRatio >= 1.2){
-    window.isRetina = true;
-    $("[data-src-2x]").each(function(){
-      if(this.tagName === "IMG"){
-        $(this).attr("src",$(this).attr("data-src-2x"));
-      } else {
-        $(this).css({"background-image":"url("+$(this).attr("data-src-2x")+")"});
-      }
-    });
-  }
-
-
-
-
-
-
-
-  //add title attribute
-  (function($){
-    var links = $('a').filter(function(){
-      return !$(this).attr('title') || $(this).attr('title') === "";
-    });
-
-    links.each(function() {
-      $(this).attr('title', $(this).text().trim());
-    });
-  })($);
-
-
-
-
-
-
-
-
-  /*
-    scroll down if on the 404 page.
-  */
-  if($('.four-oh-four').length) {
-    $('html, body').animate({
-      scrollTop: $('.four-oh-four section').offset().top
-    }, 500);
-  }
-
-})($);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var opts = {
-  lines: 13, // The number of lines to draw
-  length: 20, // The length of each line
-  width: 10, // The line thickness
-  radius: 30, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 0, // The rotation offset
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#FFA700', // #rgb or #rrggbb or array of colors
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: false, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: '50%', // Top position relative to parent
-  left: '50%' // Left position relative to parent
-};
-
-
-
-
-
-
-
-
-
-
-
-(function($){
+(function($) {
   /*
     LastFM api call and dom loads
   */
-  lfmOpts = {
+  var lfmOpts = {
     APIkey: '6a77d69fd4f528fe5101f0e2e4912e8c',
     User: 'iBrianAnders',
     limit: 24, // 1 album - 50 albums
-    period: "1month" //overall|7day|1month|3month|6month|12month
+    period: '1month' //overall|7day|1month|3month|6month|12month
   };
-
-  var target = document.getElementById('albums-spinner');
-  var spinner = new Spinner(opts).spin(target);
 
   if($('.albums').length) {
     $.ajax({
       type: 'GET',
-      url: ("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" + lfmOpts.User + "&period=" + lfmOpts.period + "&api_key=" + lfmOpts.APIkey + "&format=json&limit=" + lfmOpts.limit +"&callback=?"),
+      url: ('http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' +
+              lfmOpts.User +
+              '&period=' +
+              lfmOpts.period +
+              '&api_key=' +
+              lfmOpts.APIkey +
+              '&format=json&limit=' +
+              lfmOpts.limit  +
+              '&callback=?'),
       dataType: 'json',
-      success: function(data){
+      success: function(data) {
         if(data.topalbums === undefined) {
           $('.albums').hide();
-          $('.instagram').addClass('full');
           return;
         }
         var markup = [],
             count = 0,
             max = 12;
 
-        data.topalbums.album.forEach(function(album){
-          if(album.image[album.image.length-1]["#text"].indexOf("default") === -1 && count !== max) {
-            markup.push("<a target='_blank' href='" + album.url + "' class='album' title='" + album.name + "'><img src='" + album.image[album.image.length-1]["#text"] + "' alt='" + album.name + "'></a>");
-            count++;
+        data.topalbums.album.forEach(function(album) {
+          if(album.image[album.image.length-1]['#text'].indexOf('default') === -1 && count !== max) {
+            markup.push('<a target="_blank" href="' +
+                         album.url +
+                         '" class="album" title="' +
+                         album.name +
+                         '"><img src="' +
+                         album.image[album.image.length-1]['#text'] +
+                         '" alt="' +
+                         album.name +
+                         '"></a>');
+            count ++ ;
           }
         });
 
-        $(target).hide();
         $('.albums').append(markup.join(''));
+
+        $('.albums img').each(imageWatcher.watchImage);
+        galaxySetup();
       },
-      error: function(){
+      error: function() {
         $('.albums').hide();
-        $('.instagram').addClass('full');
       }
     });
   }
@@ -209,59 +117,51 @@ var opts = {
 
 
 
-
-
-
-
-
-
-
-
-(function($){
+(function($) {
   /*
     instagram api call and dom load
   */
-  iOpts = {
+  var iOpts = {
     APIkey: 'f42ca37467bc4296b311a70e1258c88e',
     User: '196017474'
-  }
-
-  var target = document.getElementById('instagram-spinner');
-  var spinner = new Spinner(opts).spin(target);
+  };
 
   if($('.instagram').length) {
     $.ajax({
       type: 'GET',
-      url: 'https://api.instagram.com/v1/users/' + iOpts.User + '/media/recent/?client_id=' + iOpts.APIkey,
+      url: 'https://api.instagram.com/v1/users/' +
+            iOpts.User +
+            '/media/recent/?client_id=' +
+            iOpts.APIkey,
       dataType: 'jsonp',
       success: function(response) {
         if(response.data === undefined) {
           $('.instagram').hide();
-          $('.albums').addClass('full');
           return;
         }
         var markup = [];
 
-        response.data.forEach(function(photo, index){
+        response.data.forEach(function(photo, index) {
           if(index >= 12) return;
 
-          markup.push("<a target='_blank' href='" +
-                       photo.link +
-                       "' title='" +
-                       (photo.caption == null ? "no caption" : photo.caption.text) +
-                       "'><img src='" +
-                       (window.isRetina ? photo.images.low_resolution.url : photo.images.thumbnail.url) +
-                       "' alt='" +
-                       (photo.caption == null ? "no caption" : photo.caption.text) +
-                       "'></a>");
+          markup.push('<a target="_blank" href="'  +
+                       photo.link  +
+                       '" title="'  +
+                       (photo.caption === null ? 'no caption' : photo.caption.text)  +
+                       '"><img src="'  +
+                       (window.isRetina ? photo.images.low_resolution.url : photo.images.thumbnail.url)  +
+                       '" alt="'  +
+                       (photo.caption === null ? 'no caption' : photo.caption.text)  +
+                       '"></a>');
         });
 
-        $(target).hide();
         $('.instagram').append(markup.join(''));
+
+        $('.instagram img').each(imageWatcher.watchImage);
+        galaxySetup();
       },
       error: function() {
         $('.instagram').hide();
-        $('.albums').addClass('full');
       }
     });
   }
@@ -276,61 +176,213 @@ var opts = {
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var svg = $('#svg')[0];
-  var svgns = "http://www.w3.org/2000/svg";
-  var WIDTH = (document.body.clientWidth * 2);
-  var HEIGHT = (document.body.clientHeight * 2);
-  svg.setAttributeNS(null, "viewBox", "0 0 " + WIDTH + " " + HEIGHT);
+  /*
+    makes the image_tag helper retina images work.
+  */
+  window.isRetina = false;
 
-  var stars = [];
-  var b, m;
-
-  function makeShape() {
-
-    var shape = document.createElementNS(svgns, "circle");
-    shape.setAttributeNS(null, "cx", Math.floor(Math.random() * WIDTH));
-    shape.setAttributeNS(null, "cy", Math.floor(Math.random() * HEIGHT));
-    shape.setAttributeNS(null, "r",  Math.floor(Math.random() * 4));
-    shape.setAttributeNS(null, "fill", "#eef");
-
-    if(Math.random() * 4 < 3) {
-      shape.classList.add('invisible');
-    }
-
-    svg.appendChild(shape);
-
-    stars.push(shape);
+  if(window.devicePixelRatio >= 1.2) {
+    window.isRetina = true;
+    $('[data-src-2x]').each(function() {
+      if(this.tagName === 'IMG') {
+        $(this).attr('src', $(this).attr('data-src-2x'));
+      } else {
+        $(this).css({
+          'background-image': 'url(' + $(this).attr('data-src-2x') + ')'
+        });
+      }
+    });
   }
 
-  for(var i = 0; i < ((WIDTH * HEIGHT) / 15000); i++) {
-    makeShape();
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+  //add title attribute
+  (function($) {
+    var links = $('a').filter(function() {
+      return !$(this).attr('title') || $(this).attr('title') === '';
+    });
+
+    links.each(function() {
+      console.log(this);
+      $(this).attr('title', $(this).text().trim());
+    });
+  })($);
+
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+  $('img').each(imageWatcher.watchImage);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+var galaxy = document.getElementById('galaxy');
+var svgns = 'http://www.w3.org/2000/svg';
+var WIDTH, HEIGHT;
+var stars = [];
+var b, m;
+
+
+function galaxySetup() {
+
+  WIDTH = (document.body.clientWidth * 2);
+  HEIGHT = (document.body.clientHeight * 2);
+  if(HEIGHT < window.innerHeight) {
+    HEIGHT = window.innerHeight * 2;
+  }
+  if(window.isRetina) {
+    WIDTH *= 2;
+    HEIGHT *= 2;
+  }
+  galaxy.setAttributeNS(null, 'viewBox', '0 0 ' + WIDTH + ' ' + HEIGHT);
+
+
+  var numOfStars = ((WIDTH * HEIGHT) / 15000) - stars.length;
+
+  for(var i = 0; i < numOfStars; i++ ) {
+    makeStar();
   }
 
-  setTimeout(function(){
-    svg.classList.add('on-deck');
+}
+
+
+
+function makeStar() {
+
+  var star = document.createElementNS(svgns, 'circle');
+  setStarAttributes(star);
+
+  if(Math.random() * 4 < 3) {
+    star.classList.add('invisible');
+  }
+
+  galaxy.appendChild(star);
+
+  stars.push(star);
+
+}
+
+
+
+function setStarAttributes(star) {
+  star.setAttributeNS(null, 'cx', Math.floor(Math.random() * WIDTH));
+  star.setAttributeNS(null, 'cy', Math.floor(Math.random() * HEIGHT));
+  star.setAttributeNS(null, 'r',  Math.floor(Math.random() * 4));
+
+  switch (Math.floor(Math.random() * 10)) {
+    case 0:
+      star.setAttributeNS(null, 'fill', '#FFF');
+      break;
+    case 1:
+      star.setAttributeNS(null, 'fill', '#FFE');
+      break;
+    case 2:
+      star.setAttributeNS(null, 'fill', '#FFD');
+      break;
+    case 3:
+      star.setAttributeNS(null, 'fill', '#FFC');
+      break;
+    case 4:
+      star.setAttributeNS(null, 'fill', '#FEF');
+      break;
+    case 5:
+      star.setAttributeNS(null, 'fill', '#FDF');
+      break;
+    case 6:
+      star.setAttributeNS(null, 'fill', '#FCF');
+      break;
+    case 7:
+      star.setAttributeNS(null, 'fill', '#EFF');
+      break;
+    case 8:
+      star.setAttributeNS(null, 'fill', '#DFF');
+      break;
+    default:
+      star.setAttributeNS(null, 'fill', '#CFF');
+  }
+}
+
+
+
+function showGalaxy() {
+
+  setTimeout(function() {
+    galaxy.classList.add('on-deck');
   }, 200);
 
 
-  setTimeout(function(){
-    svg.classList.add('visible');
+  setTimeout(function() {
+    galaxy.classList.add('visible');
   }, 2000);
 
-  var updateStars = setInterval(function(){
-    //y = mx+b
+
+  setInterval(function() {
+    //y = mx + b
 
     b = Math.floor(Math.random() * 10);
     m = Math.floor(Math.random() * 5) + 5;
+    var star;
 
-    for(var i = b; i < stars.length; i += m) {
+    for(var i = b; i < stars.length; i  += m) {
+      star = stars[i];
+
       if(stars[i].classList.contains('invisible')) {
-        stars[i].classList.remove('invisible');
+
+        setStarAttributes(star)
+        star.classList.remove('invisible');
+
       } else {
-        stars[i].classList.add('invisible');
+        star.classList.add('invisible');
       }
     }
 
   }, 1000);
+
+}
+
+
+
+
+
+$(document).ready(function() {
+
+
+  $(window).on('resize', galaxySetup);
+
+
+  galaxySetup();
+  showGalaxy();
+
 
 });
