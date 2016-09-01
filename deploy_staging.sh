90d52d0b1211e3bfe9434286919ddd5bbf42ff3e
Brian Anders <brian.steven.anders@gmail.com>
2016-08-31 21:24:57 -0700

    Switch to S3.

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 echo "Changing build permissions to 755"
 chmod -R 755 build/
-echo "Moving Files to EC2"
+echo "Moving Files to S3 staging"
 aws s3 sync build/ s3://staging.briananders.net --recursive
 echo "Done"
 
